9131e0f35e7e2b63857b1dbd953db5b3703b8c8a
Divran <arviddivran@gmail.com>
2018-08-30 21:17:02 +0200

    Saved the setting in localStorage to remember it across sessions
    Basically cookies

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,11 +2,15 @@
 // Controls the invert button
 (function() {
   var btn = document.getElementById("invert-btn");
-  var inverted = false;
-  btn.onclick = function() {
-    inverted = !inverted;
 
-    if (inverted) {
+  if (typeof localStorage.inverted == "undefined") {
+  	localStorage.inverted = "0";
+  }
+
+  btn.onclick = function() {
+    localStorage.inverted = (localStorage.inverted == "0" ? "1" : "0");
+
+    if (localStorage.inverted == "1") {
       document.body.classList.add( "inverted" );
     } else {
       document.body.classList.remove( "inverted" );
