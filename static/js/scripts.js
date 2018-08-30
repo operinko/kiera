cc60185dc2397dcf269974217db483428001d0f6
Divran <arviddivran@gmail.com>
2018-08-30 22:19:07 +0200

    whoops forgot to apply the setting on page load

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -7,13 +7,17 @@
   	localStorage.inverted = "0";
   }
 
-  btn.onclick = function() {
-    localStorage.inverted = (localStorage.inverted == "0" ? "1" : "0");
-
+  function apply() {
     if (localStorage.inverted == "1") {
       document.body.classList.add( "inverted" );
     } else {
       document.body.classList.remove( "inverted" );
     }
+  }
+  apply();
+
+  btn.onclick = function() {
+    localStorage.inverted = (localStorage.inverted == "0" ? "1" : "0");
+    apply();
   }
 })();
