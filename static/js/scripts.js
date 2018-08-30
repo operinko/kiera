a540e8c83ef5010258fc265ba3d4ec314b94db74
Divran <arviddivran@gmail.com>
2018-08-30 21:01:52 +0200

    Adds a color invert button to the top right
    Source for icon image:
    https://material.io/tools/icons/?search=invert&icon=invert_colors&style=baseline

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,1 +1,15 @@
-// nothing here yet.
+
+// Controls the invert button
+(function() {
+  var btn = document.getElementById("invert-btn");
+  var inverted = false;
+  btn.onclick = function() {
+    inverted = !inverted;
+
+    if (inverted) {
+      document.body.classList.add( "inverted" );
+    } else {
+      document.body.classList.remove( "inverted" );
+    }
+  }
+})();
